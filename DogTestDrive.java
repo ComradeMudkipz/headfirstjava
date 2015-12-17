1f74ad0a06311edc7a36cffbe8409461efedd9a1
ComradeMudkipz <comrademudkipz@live.com>
2015-12-17 15:29:23 -0800

    Added new instance variables and methods.

--- DogTestDrive.java
+++ DogTestDrive.java
@@ -1,19 +1,30 @@
 class Dog {
 
     int size;
-    String breed;
     String name;
 
     void bark() {
-        System.out.println("Ruff! Ruff!");
+        if (size > 60) {
+            System.out.println("Wooof! Wooof!");
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
     }
 }
 
 class DogTestDrive {
     public static void main (String[] args) {
-        
-        Dog d = new Dog();
-        d.size = 40;
-        d.bark();
+        Dog one = new Dog();
+        one.size = 70;
+        Dog two = new Dog();
+        two.size = 8;
+        Dog three = new Dog();
+        three.size = 35;
+
+        one.bark();
+        two.bark();
+        three.bark();
     }
 }
