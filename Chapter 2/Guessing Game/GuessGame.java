b5ef283171e15322e6915551728edb62f3397fa9
ComradeMudkipz <comrademudkipz@live.com>
2016-01-12 13:34:35 -0800

    Added comments.

--- Chapter 2/Guessing Game/GuessGame.java
+++ Chapter 2/Guessing Game/GuessGame.java
@@ -1,31 +1,40 @@
 public class GuessGame {
+    // GuessGame has three instance variables for three Player objects
     Player p1;
     Player p2;
     Player p3;
 
     public void startGame() {
+        // create three Player objects and assign them to the three Player
+        // instance variables
         p1 = new Player();
         p2 = new Player();
         p3 = new Player();
 
+        // declare three variables to hold the three guesses the Players make
         int guessp1 = 0;
         int guessp2 = 0;
         int guessp3 = 0;
 
+        // declare three variables to hold a true or false based on the player's answer
         boolean p1isRight = false;
         boolean p2isRight = false;
         boolean p3isRight = false;
 
+        // make a 'target' number that the players have to guess
         int targetNumber = (int) (Math.random() * 10);
         System.out.println("I'm thinking of a number between 0 and 9...");
 
         while(true) {
             System.out.println("Number to get is " + targetNumber);
 
+            // call each player's guess() method
             p1.guess();
             p2.guess();
             p3.guess();
 
+            // get each player's guess (the result of their guess() method running)
+            // by accessing the number variable of each player
             guessp1 = p1.number;
             System.out.println("Player one guessed " + guessp1);
 
@@ -35,6 +44,9 @@ public class GuessGame {
             guessp3 = p3.number;
             System.out.println("Player three guessed " + guessp3);
 
+            // check each player's guess to see if it matches the target number. If
+            // a player is right, then set that player's variable to be true
+            // (remember, we set it false by default)
             if (guessp1 == targetNumber) {
                 p1isRight = true;
             }
@@ -45,6 +57,8 @@ public class GuessGame {
                 p3isRight = true;
             }
 
+            // if player one OR player two OR palyer three is right...
+            // (the || operator means OR)
             if (p1isRight || p2isRight || p3isRight) {
 
                 System.out.println("We have a winner!");
@@ -54,6 +68,7 @@ public class GuessGame {
                 System.out.println("Game is over.");
                 break;  // game over, so break out of the loop
 
+            // otherwise, stay in the loop and ask the players for another guess.
             } else {
                 // we must keep going because nobdy got it right!
                 System.out.println("Players will have to try again.");
