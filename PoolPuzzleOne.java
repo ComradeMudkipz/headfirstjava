b70fef1a8d26c23b52f54bda4f52fca3a1088b86
ComradeMudkipz <comrademudkipz@live.com>
2016-01-12 05:10:23 -0800

    Added comments

--- PoolPuzzleOne.java
+++ PoolPuzzleOne.java
@@ -1,9 +1,11 @@
 class PoolPuzzleOne {
     public static void main(String [] args) {
+        // declare an integer variable named 'x' and give it the value 0
         int x = 0;
 
         while () {
             ;
+            // if 'x' is less than 1, do this
             if (x < 1) {
             ;
             }
@@ -12,6 +14,7 @@ class PoolPuzzleOne {
                 ;
                 ;
             }
+            // if 'x' is equal to 1, do this
             if (x == 1) {
                 ;
             }
