c4c34c077dacc1d48de7d8879352c2bbc6edf264
ComradeMudkipz <comrademudkipz@live.com>
2016-01-12 18:38:19 -0800

    Added comments.

--- Chapter 2/CodeMagnets.java
+++ Chapter 2/CodeMagnets.java
@@ -1,13 +1,19 @@
+// TODO: Add comments
+
 class DrumKit {
 
+    // declare a boolean variable named "snare" and set it as true
     boolean snare = true;
 
+    // declare a boolean variable named "topHat" and set it as true
     boolean topHat = true;
 
+    // create a method named "playSnare" and do this
     void playSnare() {
         System.out.println("bang bang ba-bang");
     }
 
+    // create a method named "playTopHat" and do this
     void playTopHat () {
         System.out.println("ding ding da-ding");
     }
@@ -16,11 +22,17 @@ class DrumKit {
 class DrumKitTestDrive {
     public static void main(String [] args) {
 
+        // create a new object with the pre-defined variables
         DrumKit d = new DrumKit();
+        
+        // call the "playSnare" method
         d.playSnare();
+        // set the boolean variable "snare" as false
         d.snare = false;
+        // call the "playTopHat" method
         d.playTopHat();
         if (d.snare == true) {
+            // call the method "playSnare" if the method is true
             d.playSnare();
         }
     }
